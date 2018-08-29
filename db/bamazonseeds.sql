d1aa180638ffe528b55ebefcb3b67f67b207cbc7
mpowalisz <markpowalisz@gmail.com>
2018-08-29 18:51:03 -0400

    Corrected inconsistencies in schema and seeds

---
+++ db/bamazonseeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('XBOX ONE X', 499.99, 5, 'CONSOLE', 400, 'XBOX ONE');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('PLAYSTATION 4 PRO', 399.99, 5, 'CONSOLE', 300, 'PS4');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('NINTENDO SWITCH', 299.99, 5, 'CONSOLE', 200, 'SWITCH');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('COD WWII', 59.99, 10, 'GAMES', 40, 'XBOX ONE');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('SUPER MARIO ODYSSEY', 59.99, 10, 'GAMES', 40, 'SWITCH');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('ASSASSINS CREED: ORIGINS', 59.99, 10, 'GAMES', 40, 'PS4');
+
+INSERT INTO products (item, price, stock, department, cost, platform)
+VALUES ('DUALSHOCK 4 CONTROLLER', 59.99, 10, 'ACCESSORIES', 40, 'PS4');
